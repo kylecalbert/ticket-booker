8ae3231ec3af42d35b285e88892641049ff26c92
kylecalbert <kylecalbert@gmail.com>
2023-10-04 11:13:19 +0100

    minor changes

--- src/TicketContext/TicketContext.js
+++ src/TicketContext/TicketContext.js
@@ -6,6 +6,11 @@ export const TicketProvider = ({ children }) => {
   const [category, setCategory] = useState('All Tickets');
   const [tickets, setTickets] = useState([]);
   const [originalTickets, setOriginalTickets] = useState([]);
+  const [ticketCount, setTicketCount] = useState([
+    { Travel: 0, Concert: 0, Movie: 0, allTickets: 0 },
+  ]);
+
+  ////create state of the different ticket types, loop through the data and filter each the tickets, then count
 
   useEffect(() => {
     async function fetchData() {
